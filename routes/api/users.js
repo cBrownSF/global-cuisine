c44b5fe86236d4b8706fefaa0d8ad1c8f74c81b7
your name here <your_email_hereroelliga@yahoo.com>
2021-12-06 13:49:40 -0800

    validations on routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,14 +5,21 @@ const router = express.Router();
 const keys = require('../../config/keys');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const validateRegisterInput = require('../../validation/register');
-const validateLoginInput = require('../../validation/login');
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 router.get("/test", (req, res) => {
     res.json({ msg: "This is the user route " });
   });
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+
     User.findOne({ email: req.body.email })
       .then(user => {
         if (user) {
@@ -38,6 +45,12 @@ router.post('/register', (req, res) => {
   })
 
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if(!isValid){
+    return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
