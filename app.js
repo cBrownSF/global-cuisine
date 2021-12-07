9e45b94229502446199c668303c8e697377c7fa2
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-12-06 21:51:33 -0800

    first commit from branch

--- app.js
+++ app.js
@@ -4,26 +4,26 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const User = require("./models/User");
 const users = require("./routes/api/users");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
 
 app.get("/", (req, res) => {
   const user = new User({
     username: "jim",
     email: "jim@jim.com",
-    password: "jim1234"
-  })
-  user.save()
-  res.send("helloword");
-})
-
+    password: "jim1234",
+  });
+  user.save();
+  res.send("helloworld");
+});
 
 const port = process.env.PORT || 5000;
 
