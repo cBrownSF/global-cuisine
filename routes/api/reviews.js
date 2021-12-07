7816ce401b697b17a07870b9da354206c28a0718
your name here <your_email_hereroelliga@yahoo.com>
2021-12-07 11:06:02 -0800

    Review routes setup

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -2,4 +2,33 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Review = require("../../models/Review");
-const validateListingInput = require("../../validations/reviews")
+const validateReviewInput = require("../../validations/reviews")
+
+router.get("/", (req, res) => {
+    Review.find()
+      .then((reviews) => res.json(reviews))
+      .catch((err) => res.status(404).json({ noreviewsfound: "No review found" }));
+  });
+  
+  router.post("/", 
+      passport.authenticate("jwt", {session: false}),
+      (req, res) => {
+          const {isValid, errors} = validateReviewInput(req.body);
+
+          if(!isValid){
+              return res.status(400).json(errors);
+          }
+          const newReview = new Review({
+              author_id: req.user.id,
+              listing_id: req.listing.id,
+              reviewer_name: req.body.reviewer_name,
+              score: req.body.score,
+              review: req.body.review
+
+          });
+          newReview.save().then((review) => res.json(review))
+      }
+  )
+  
+  
+  module.exports = router;
