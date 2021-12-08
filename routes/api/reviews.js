3e66dde37a2c6f11d6eee79894e0d10d9c7e505e
your name here <your_email_hereroelliga@yahoo.com>
2021-12-07 18:00:01 -0800

    reviews routes

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -13,6 +13,29 @@ router.get("/", (req, res) => {
       .then((reviews) => res.json(reviews))
       .catch((err) => res.status(404).json({ noreviewsfound: "No review found" }));
   });
+
+  router.get("/user/:user_id", (req, res) => {
+    Review
+    .find({ user: req.params.user_id })
+    .then(reviews => res.json(reviews))
+    .catch(err => res.status(400).json(err));
+})
+
+router.get("/listing/:listing_id", (req, res) => {
+    Review
+    .find({ user: req.params.user_id })
+    .then(reviews => res.json(reviews))
+    .catch(err => res.status(400).json(err));
+})
+
+
+router.get("/:id", (req, res) => {
+    Listing
+    .findById(req.params.id)
+    .then(review => res.json(review))
+    .catch(err => res.status(400).json(err));
+})
+
   
   router.post("/", 
       passport.authenticate("jwt", {session: false}),
@@ -33,5 +56,40 @@ router.get("/", (req, res) => {
           newReview.save().then((review) => res.json(review))
       }
   )
+
+  router.patch(
+    '/:id/update',
+    passport.authenticate('jwt', {session:false}),
+    (req, res) => {
+        const {isValid, errors} = validateReviewInput(req.body);
+        if(!isValid){
+            return res.status(400).json(errors);
+        }
+        Review.findById(req.params.id).then((review) => {
+            if(!review){
+                errors.review = "No review found with that ID";
+                return res.status(404).json(errors);
+            }else{
+                review.review = req.body.review,
+                review.score = req.body.score,
+                review.reviewer_name = req.body.reviewer_name;
+                review.save().then((review) => res.json(review));
+            }
+        })
+    }
+)
+
+router.delete(
+    "/:id/delete",
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        Review.findById(req.params.id).then((review) => {
+                review.deleteOne({_id: req.params.id}).then(() => {
+                    return res.status(200).json({success: "Successfully deleted"})
+                })
+        })
+    }
+)
+
   
   module.exports = router;
