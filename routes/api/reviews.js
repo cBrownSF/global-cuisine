3384491c7fe5a6d1e0a3c7372f0a8203ca935274
your name here <your_email_hereroelliga@yahoo.com>
2021-12-08 11:56:30 -0800

    review backend bugs

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const passport = require("passport");
 const Review = require("../../models/Review");
 const validateReviewInput = require("../../validations/reviews")
@@ -14,31 +15,37 @@ router.get("/", (req, res) => {
       .catch((err) => res.status(404).json({ noreviewsfound: "No review found" }));
   });
 
-  router.get("/user/:user_id", (req, res) => {
+  router.get("/:reviewId", (req, res) => {
+    Review.findById(req.body.id)
+      .then((review) => res.json(review))
+      .catch((err) => res.status(404).json({ noreviewfound: "No review found with that Id" }));
+  });
+
+  router.get("/user/:userId", (req, res) => {
     Review
-    .find({ user: req.params.user_id })
+    .find({ userId: req.params.userId })
+    .then(reviews => res.json(reviews))
+    .catch((err) => res.status(404).json({ noreviewsfound: "This user has not created any reviews" }));
+})
+
+router.get("/listing/:listingId", (req, res) => {
+    Review
+    .find({ listingId: req.params.listingId })
     .then(reviews => res.json(reviews))
     .catch(err => res.status(400).json(err));
 })
 
-router.get("/listing/:listing_id", (req, res) => {
-    Review
-    .find({ user: req.params.user_id })
-    .then(reviews => res.json(reviews))
-    .catch(err => res.status(400).json(err));
-})
 
-
-router.get("/:id", (req, res) => {
-    Listing
-    .findById(req.params.id)
-    .then(review => res.json(review))
-    .catch(err => res.status(400).json(err));
-})
+// router.get("/:id", (req, res) => {
+//     Listing
+//     .findById(req.params.id)
+//     .then(review => res.json(review))
+//     .catch(err => res.status(400).json(err));
+// })
 
   
-  router.post("/", 
-      passport.authenticate("jwt", {session: false}),
+  router.post("/:listingId/reviews", 
+    //   passport.authenticate("jwt", {session: false}),
       (req, res) => {
           const {isValid, errors} = validateReviewInput(req.body);
 
@@ -46,8 +53,8 @@ router.get("/:id", (req, res) => {
               return res.status(400).json(errors);
           }
           const newReview = new Review({
-              author_id: req.user.id,
-              listing_id: req.listing.id,
+              authorId: req.user.id,
+              listingId: req.params.listingId,
               reviewer_name: req.body.reviewer_name,
               score: req.body.score,
               review: req.body.review
