a1b2aba678b6e6afabaf5de61e2e4a7ac5883614
Cal Brown <calsbrown4@gmail.com>
2021-12-09 11:32:22 -0800

    Updated with cleaner code

--- frontend/src/components/recipe/recipe_show.js
+++ frontend/src/components/recipe/recipe_show.js
@@ -12,72 +12,31 @@ class RecipeShow extends React.Component {
       this.props.receiveListing(this.props.match.params.listingId);
     }
   }
-  hideButton() {
-    if (this.props.currentUser.id !== this.props.listing.host_id) {
-      return (
-       <p><Link to={`/listings/${this.props.listing.id}/edit`}>Edit</Link></p>
-      )
-    } else {
-      return <p>not a user</p>
-    }
-  }
-
-  onDelete() {
-    if (this.props.currentUser.id !== this.props.listing.host_id) {
-      this.props.deleteListing(this.props.listing.id)
-    } else {
-      return <p>not a user</p>
-    }
-  }
-  // backButton(){
-  //   <Redirect to ={`/recipes`}/>
-  // }
-
+ 
   render() {
     if (!this.props.listing) {
       return null;
     }
-debugger;
+
     const listing = this.props.listing
-    if (this.props.currentUser.id === this.props.listing.author_id){
-      debugger;
-      return (
-  <div>
-    <h1>Show page</h1>
-      <p><Link to={`/recipes`}>Back to Recipes</Link></p>
-      <p><Link to={`/recipes/${this.props.listing._id}/edit`}>Edit</Link></p>
-      <ul>
-        <li>{listing.name}</li>
-        <li>{listing.ingredients}</li>
-        <li>{listing.details}</li>
-        <li>{listing.country}</li>
-        <li>{listing.title}</li>
-        <li>{listing.difficulty}</li>
-        <li>{listing.instruction}</li>
-        <li>{listing.picture}</li>
-        </ul>
-    </div>
-  )
-      }else{
-        debugger;
-        return(
-          <div>
-          <h1>Show page</h1>
-      <p><Link to={`/recipes`}>Back to Recipes</Link></p>
-      <p>    </p>
-      <ul>
-        <li>{listing.name}</li>
-        <li>{listing.ingredients}</li>
-        <li>{listing.details}</li>
-        <li>{listing.country}</li>
-        <li>{listing.title}</li>
-        <li>{listing.difficulty}</li>
-        <li>{listing.instruction}</li>
-        <li>{listing.picture}</li>
-        </ul>
-    </div >
-        )
-      }
+
+    return (
+      <div>
+        <h1>Show page</h1>
+          <p><Link to={`/recipes`}>Back to Recipes</Link></p>
+          <p>{this.props.currentUser.id === listing.author_id ? <Link to={`/recipes/${listing._id}/edit`}>Edit</Link> : "    "}</p>
+          <ul>
+            <li>{listing.name}</li>
+            <li>{listing.ingredients}</li>
+            <li>{listing.details}</li>
+            <li>{listing.country}</li>
+            <li>{listing.title}</li>
+            <li>{listing.difficulty}</li>
+            <li>{listing.instruction}</li>
+            <li>{listing.picture}</li>
+          </ul>
+      </div>
+    )
   }
 }
    
