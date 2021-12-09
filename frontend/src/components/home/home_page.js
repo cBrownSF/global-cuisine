3602b577cfbe87475d29c58b712faf1361222253
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-12-08 22:48:02 -0800

    added more nav css

--- frontend/src/components/home/home_page.js
+++ frontend/src/components/home/home_page.js
@@ -1,12 +1,11 @@
 import React from "react";
-import NavBarContainer from "../nav/navbar_container";
 import "./home.css";
 
 class HomePage extends React.Component {
   render() {
     return (
       <div classname="outer-div-home">
-        
+        home
       </div>
     );
   }
