64c40a17c87531865981e524be5120a142ce8025
Cal Brown <calsbrown4@gmail.com>
2021-12-09 17:19:37 -0800

    show page

--- frontend/src/components/recipe/recipe_show.js
+++ frontend/src/components/recipe/recipe_show.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import CreateReviewFormContainer from '../review/review_form_create_container'
-
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import "./recipe_show.css";
 class RecipeShow extends React.Component {
   componentDidMount() {
     this.props.receiveListing(this.props.match.params.listingId);
@@ -16,23 +15,56 @@ class RecipeShow extends React.Component {
       return null;
     }
     const listing = this.props.listing
-    debugger
     return (
       <div>
-        <h1>Show page</h1>
-          <p><Link to={`/recipes`}>Back to Recipes</Link></p>
-          <p>{this.props.currentUser.id === listing.author_id ? <Link to={`/recipes/${listing._id}/edit`}>Edit</Link> : "    "}</p>
-          <ul>
-            <li>{listing.name}</li>
-            <li>{listing.ingredients}</li>
-            <li>{listing.details}</li>
-            <li>{listing.country}</li>
-            <li>{listing.title}</li>
-            <li>{listing.difficulty}</li>
-            <li style = {{whiteSpace: 'pre-wrap'}}>{listing.instruction}</li>
-            <li>{listing.picture}</li>
-          </ul> 
-         <h1> <CreateReviewFormContainer listing_id={listing._id} /></h1>
+        {/* <p><Link to={`/listings/${this.props.listing._id}/edit`}>Edit</Link></p> */}
+        {/* <ul>
+        <li>{listing.name}</li>
+        <li>{listing.ingredients}</li>
+        <li>{listing.details}</li>
+        <li>{listing.country}</li>
+        <li>{listing.title}</li>
+        <li>{listing.difficulty}</li>
+        <li>{listing.instruction}</li>
+        <li>{listing.picture}</li>
+        </ul> */}
+        {/* <p>
+        {this.props.currentUser.id === listing.author_id ? (
+          <Link to={`/recipes/${listing._id}/edit`}>Edit</Link>
+        ) : (
+          ""
+        )}
+      </p> */}
+        <div className="right-left">
+          <div className="left-show">
+            <div>
+              <img src={listing.picture} className="show-img" alt="food"></img>
+            </div>
+            <div>
+              <p className="listing-detail">{listing.details}</p>
+            </div>
+            <div className="show-author">
+              <p className="show-author-p">Recipe by: {listing.name}</p>
+            </div>
+          </div>
+          <div className="right-show">
+            <div className="show-title">
+              <p className="show-title-p">{listing.title}</p>
+            </div>
+            <div className='below-title'>
+              <div
+                className="lishing-show-ingredient"
+              >
+                <div className="ingredients-word">Ingredients</div>
+                <div className="show-ingredient-inner" style={{ whiteSpace: "pre-wrap" }}>{listing.ingredients}</div>
+              </div>
+              <div>
+                <div style={{ whiteSpace: "pre-wrap" }}>{listing.instruction}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bottom-show"></div>
       </div>
     );
   }
