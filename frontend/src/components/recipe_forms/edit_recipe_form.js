e492f4d4a08233d3ead500debd69581938feeae8
Cal Brown <calsbrown4@gmail.com>
2021-12-14 15:33:15 -0800

    edit form

--- frontend/src/components/recipe_forms/edit_recipe_form.js
+++ frontend/src/components/recipe_forms/edit_recipe_form.js
@@ -4,27 +4,33 @@ import { withRouter } from "react-router-dom";
 class EditRecipeForm extends React.Component {
   constructor(props) {
     super(props)
+    // this.state = {
+    //   name: listing.name,
+    //   author_id: this.props.currentUser.id,
+    //   ingredients: listing.ingredients,
+    //   instruction: listing.instruction,
+    //   details: listing.details,
+    //   difficulty: listing.difficulty,
+    //   servings: listing.servings,
+    //   title: listing.title,
+    //   picture:
+    //     "https://global-cuisine.s3.us-west-1.amazonaws.com/worldflags.jpeg",
+    //   country: "Italy",
+    //   editId: this.props.listing._id,
+    // };
 
-    const listing = this.props.listing
-    this.state = {
-      name: listing.name,
-      author_id: this.props.currentUser.id,
-      ingredients: listing.ingredients,
-      instruction: listing.instruction,
-      details: listing.details,
-      difficulty: listing.difficulty,
-      servings: listing.servings,
-      title: listing.title,
-      picture:
-        "https://global-cuisine.s3.us-west-1.amazonaws.com/worldflags.jpeg",
-      country: "Italy",
-      editId: this.props.listing._id,
-    };
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleKeyPress = this.handleKeyPress.bind(this)
   }
   componentDidMount() {
-    this.props.receiveListing(this.props.match.params.listingId);
+    this.props.receiveListing(this.props.match.params.listingId)
+    .then(listing=>{
+      debugger;
+     return this.setState({
+       name: listing.listing.data.name,
+       ingredients: listing.listing.data.ingredients
+      })
+    });
   }
 
   componentDidUpdate() {
@@ -64,80 +70,83 @@ class EditRecipeForm extends React.Component {
     )
   }
   render() {
+    console.log(this.state)
     if (!this.props.listing) {
       return null;
     }
     return (
-      <div>
-        {/* <p> <Link to={`/listings/${listing.id}/edit`}>Edit</Link></p> */}
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <h1 id="title">Edit your recipe</h1>
-            <br />
-            <input type="text"
-              value={this.state.title}
-              onChange={this.handleInput('title')}
-              placeholder="title of recipe"
-            />
-            <br />
-            <input type="text" onKeyPress={this.handleKeyPress('ingredients')}
-              value={this.state.ingredients}
-              onChange={this.handleInput('ingredients')}
-              placeholder="ingredients"
-            />Add or delete ingredients
-            <br />
-            <input type="text"
-              value={this.state.servings}
-              onChange={this.handleInput('servings')}
-              placeholder="how many servings"
-            />How many servings?
-            <br />
-            <input type="text"
-              value={this.state.picture}
-              onChange={this.handleInput('picture')}
-              placeholder="Add a picture"
-            />Upload a new photograph
-            <br />
-            <input type="text"
-              value={this.state.name}
-              onChange={this.handleInput('name')}
-              placeholder="your name"
-            />Change your name!
-            <br />
-            <input type="text"
-              value={this.state.details}
-              onChange={this.handleInput('details')}
-              placeholder="Add a succinct description"
-            />Update your blurb
-            <br />
-            <textarea onKeyPress={this.handleKeyPress('instruction')}
-              value={this.state.instruction}
-              onChange={this.handleInput('instruction')}
-              placeholder="Add your instructions here"
-            />Improve your instructions!
-            <br />
-            <label>Country
-              <select value={this.state.country} onChange={this.handleInput('country')}>
-                <option value="Italy">Italy</option>
-                <option value="France">France</option>
-                <option value="India">India</option>
-              </select>
-            </label>
-            <label>Difficulty
-              <select value={this.state.difficulty} onChange={this.handleInput('difficulty')}>
-                <option value="Easy">Easy</option>
-                <option value="Medium">Medium</option>
-                <option value="Hard">Hard</option>
-              </select>
-            </label>
-            <br />
-            <button onClick={() => this.props.deleteListing(this.props.listing._id)}>delete listing</button>
-            <input type="submit" value="Submit" />
-            {this.renderErrors()}
-          </div>
-        </form>
-      </div>
-    );
+     <p>hello</p>
+  //     <div>
+  //       {/* <p> <Link to={`/listings/${listing.id}/edit`}>Edit</Link></p> */}
+  //       <form onSubmit={this.handleSubmit}>
+  //         <div>
+  //           <h1 id="title">Edit your recipe</h1>
+  //           <br />
+  //           <input type="text"
+  //             value={this.state.title}
+  //             onChange={this.handleInput('title')}
+  //             placeholder="title of recipe"
+  //           />
+  //           <br />
+  //           <input type="text" onKeyPress={this.handleKeyPress('ingredients')}
+  //             value={this.state.ingredients}
+  //             onChange={this.handleInput('ingredients')}
+  //             placeholder="ingredients"
+  //           />Add or delete ingredients
+  //           <br />
+  //           <input type="text"
+  //             value={this.state.servings}
+  //             onChange={this.handleInput('servings')}
+  //             placeholder="how many servings"
+  //           />How many servings?
+  //           <br />
+  //           <input type="text"
+  //             value={this.state.picture}
+  //             onChange={this.handleInput('picture')}
+  //             placeholder="Add a picture"
+  //           />Upload a new photograph
+  //           <br />
+  //           <input type="text"
+  //             value={this.state.name}
+  //             onChange={this.handleInput('name')}
+  //             placeholder="your name"
+  //           />Change your name!
+  //           <br />
+  //           <input type="text"
+  //             value={this.state.details}
+  //             onChange={this.handleInput('details')}
+  //             placeholder="Add a succinct description"
+  //           />Update your blurb
+  //           <br />
+  //           <textarea onKeyPress={this.handleKeyPress('instruction')}
+  //             value={this.state.instruction}
+  //             onChange={this.handleInput('instruction')}
+  //             placeholder="Add your instructions here"
+  //           />Improve your instructions!
+  //           <br />
+  //           <label>Country
+  //             <select value={this.state.country} onChange={this.handleInput('country')}>
+  //               <option value="Italy">Italy</option>
+  //               <option value="France">France</option>
+  //               <option value="India">India</option>
+  //             </select>
+  //           </label>
+  //           <label>Difficulty
+  //             <select value={this.state.difficulty} onChange={this.handleInput('difficulty')}>
+  //               <option value="Easy">Easy</option>
+  //               <option value="Medium">Medium</option>
+  //               <option value="Hard">Hard</option>
+  //             </select>
+  //           </label>
+  //           <br />
+  //           <button onClick={() => this.props.deleteListing(this.props.listing._id)}>delete listing</button>
+  //           <input type="submit" value="Submit" />
+  //           {this.renderErrors()}
+  //         </div>
+  //       </form>
+  //     </div>
+  //   );
+    )
   }
 }
 
