3dcbc98f3805e635dad394627a400a044ff773e4
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-12-13 21:59:48 -0800

    done with profile for adding user recipe

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -18,61 +18,52 @@ class Profile extends React.Component {
       })
       return (
         <div className="profile-outer">
+          <div className="profile-username">
+            <p>Welcome {currentUser.username}!!</p>
+          </div>
           <div>
-            <div>
-              {userRecipes.length === 0 ? (
-                <div className="no-recipe-outer">
-                  <div className="username-no-recipe">
-                    <p className="p-username-no-recipe">
-                      Welcome {currentUser.username}
+            {userRecipes.length === 0 ? (
+                <div className="detail-no-recipe">
+                  <div className="not-detail">
+                    <p className="p-not-detail">
+                      You have not created any recipe yet. Please click below to
+                      create your own recipe.
                     </p>
                   </div>
-                  <div className="detail-no-recipe">
-                    <div className="not-detail">
-                      <p className="p-not-detail">
-                        You have not created any recipe yet. Please click below
-                        to create your own recipe.
-                      </p>
-                    </div>
-                    <div className="link-create-profile">
-                      <Link to="/recipes/new" className="l-create-recipe">
-                        Create Recipe
-                      </Link>
-                    </div>
+                  <div className="link-create-profile">
+                    <Link to="/recipes/new" className="l-create-recipe">
+                      Create Recipe
+                    </Link>
                   </div>
                 </div>
-              ) : (
-                <div className="yes-recipe-outer">
-                  <div>
-                    <p>Welcome {currentUser.username}</p>
-                  </div>
-                  <div>
-                    <p>Your Recipe</p>
-                  </div>
-                  {userRecipes.map((listing, index) => (
-                    <div key={index + "b"} className="user-recipe">
-                      <Link
-                        to={`/recipes/${listing._id}`}
-                        className="link-profile"
-                      >
-                        <div className="profile-recipe-title-image">
-                          <div className="div-image-profile">
-                            <img
-                              src={listing.picture}
-                              alt="food-pic"
-                              className="images-profile"
-                            ></img>
-                          </div>
-                          <div className="listing-title-profile">
-                            <p className="title-profile">{listing.title}</p>
-                          </div>
+            ) : (
+              <div className="yes-recipe-outer">
+                <div className="your-recipe">
+                  <p className="p-your-recipe">Your Recipe</p>
+                </div>
+                {userRecipes.map((listing, index) => (
+                  <div key={index + "b"} className="user-recipe">
+                    <Link
+                      to={`/recipes/${listing._id}`}
+                      className="link-profile"
+                    >
+                      <div className="profile-recipe-title-image">
+                        <div className="div-image-profile">
+                          <img
+                            src={listing.picture}
+                            alt="food-pic"
+                            className="images-profile"
+                          ></img>
                         </div>
-                      </Link>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
+                        <div className="listing-title-profile">
+                          <p className="title-profile">{listing.title}</p>
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       );
