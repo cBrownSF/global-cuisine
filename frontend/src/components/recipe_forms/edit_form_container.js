304a04da7bf8336f8cf29bffff6df74fce3e502e
Cal Brown <calsbrown4@gmail.com>
2021-12-16 15:58:57 -0800

    Final Forms

--- frontend/src/components/recipe_forms/edit_form_container.js
+++ frontend/src/components/recipe_forms/edit_form_container.js
@@ -1,20 +1,28 @@
-import { connect } from 'react-redux';
-import { updateListing, deleteListing, getListing} from "../../actions/listing_actions";
-import EditForm from './edit_recipe_form';
-import { withRouter } from 'react-router';
-const mapStateToProps = (state,ownProps) => {
+import { connect } from "react-redux";
+import {
+  updateListing,
+  deleteListing,
+  getListing,
+  removeListingErrors,
+} from "../../actions/listing_actions";
+import EditForm from "./edit_recipe_form";
+import { withRouter } from "react-router";
+const mapStateToProps = (state, ownProps) => {
   return {
+    errors: state.errors.listing,
     currentUser: state.session.user,
-    formType: 'update',
-    listing: state.listings[ownProps.match.params.listingId]
-  }
-}
-
+    formType: "update",
+    listing: state.listings[ownProps.match.params.listingId],
+  };
+};
 const mapDispatchtoProps = (dispatch) => {
   return {
-    submitForm: id => dispatch(updateListing(id)),
-    deleteListing: id => dispatch(deleteListing(id)),
-    receiveListing: id => dispatch(getListing(id)),
-  }
-}
-export default withRouter(connect(mapStateToProps, mapDispatchtoProps)(EditForm))
+    submitForm: (id) => dispatch(updateListing(id)),
+    deleteListing: (id) => dispatch(deleteListing(id)),
+    receiveListing: (id) => dispatch(getListing(id)),
+    clearErrors: () => dispatch(removeListingErrors()),
+  };
+};
+export default withRouter(
+  connect(mapStateToProps, mapDispatchtoProps)(EditForm)
+);
