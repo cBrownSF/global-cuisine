2662071636519ef4712b8c75cc61c202d8924eb8
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-12-16 18:45:58 -0800

    working on like

--- frontend/src/components/likes/like_container.js
+++ frontend/src/components/likes/like_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { likeListing, removeLikeErrors } from "../../actions/like_actions";
+import { likeListing, removeLikeErrors, getLikes } from "../../actions/like_actions";
 import LikeForm from './like_form';
 
 
@@ -7,17 +7,20 @@ const mSTP = (state, ownProps) => {
     return {
     like: {
         liker_id: state.session.user.id,
-        listing_id: ownProps.listingId
+        listing_id: ownProps.listingId,
     },
     formType: 'Like Recipe',
     currentUser: state.session.user,
     listing: ownProps.listing,
-    errors: Object.values(state.errors.review)
+    errors: Object.values(state.errors.review),
+    likes: Object.values(state.likes),
+    listings: Object.values(state.listings)
     }
 }
 
 const mDTP = (dispatch) => ({
   submitLike: (like) => dispatch(likeListing(like)),
-  removeLikeErrors: () => dispatch(removeLikeErrors())
+  removeLikeErrors: () => dispatch(removeLikeErrors()),
+  getLikes: () => dispatch(getLikes())
 });
 export default connect(mSTP, mDTP)(LikeForm)
--- frontend/src/components/likes/like_index.js
+++ frontend/src/components/likes/like_index.js
@@ -7,19 +7,19 @@ class LikeIndex extends React.Component {
     if (!this.props.likes) return null;
     const { likes, listingId, deleteLike, currentUserId } = this.props;
     let likeListingArray = [];
-    let count = 1;
+    let count = 0;
     likes.forEach((like) => {
       if (like.listing_id === listingId) {
-        likeListingArray.push(like);
+        count += 1
       }
     });
 
-    for (let i = 0; i < likeListingArray.length; i++) {
-      if (likeListingArray[i].liker_id !== likeListingArray[i + 1].liker_id) {
-        count += 1;
-      }
-    }
-
+    // for (let i = 0; i < likeListingArray.length; i++) {
+    //   debugger
+    //   if (likeListingArray[i].liker_id !== likeListingArray[i + 1].liker_id) {
+    //     count += 1;
+    //   }
+    // }
 
     return (
       <div className="likes-list-class">
