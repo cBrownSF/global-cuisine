e2ffd6733d86614fc300f4334ba8f7e18855d538
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-12-17 01:36:48 -0800

    like not working

--- frontend/src/components/likes/like_index.js
+++ frontend/src/components/likes/like_index.js
@@ -9,21 +9,19 @@ class LikeIndex extends React.Component {
              if (!this.props.likes) return null;
              const { likes, listingId, deleteLike, currentUserId } = this.props;
              let likeListingArray = [];
-             let count = 0;
+             let count = 1;
              likes.forEach((like) => {
                if (like.listing_id === listingId) {
                  likeListingArray.push(like);
                }
              });
-
                for (let i = 0; i < likeListingArray.length-1; i++) {
                  if (
                    likeListingArray[i].liker_id !== likeListingArray[i + 1].liker_id
                  ) {
                    count += 1;
                  }
-               }
-
+                }
              return <div className="likes-list-class">{count}</div>;
            }
 }
