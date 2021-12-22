106c73db47e9e8e6cc3f73fd73aeac0ae4c2f450
Cal Brown <calsbrown4@gmail.com>
2021-12-22 13:28:17 -0800

    Dot Env Changes

--- image_upload.js
+++ image_upload.js
@@ -1,31 +1,33 @@
-require('dotenv').config();
-const fs = require('fs');
+
 const AWS= require('aws-sdk');
-const express = require('express');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKey = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+// const bucketName = process.env.AWS_BUCKET_NAME;
+// const region = process.env.AWS_BUCKET_REGION;
+// const accessKey = process.env.AWS_ACCESS_KEY;
+// const secretAccessKey = process.env.AWS_SECRET_KEY;
 
-const app = express()
-const s3 = new AWS.S3({ 
-  secretAccessKey,
-  accessKey,
-  region
+AWS.config.update({
+  secretAccessKey: process.env.S3_SECRET_KEY,
+  accessKeyId:process.env.S3_ACCESS_KEY,
+  region: 'us-west-1'
 })
-
-var upload = multer({
+const s3 = new AWS.S3();
+console.log('hello')
+console.log(process.env.S3_SECRET_KEY)
+console.log(process.env.S3_ACCESS_KEY,)
+const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: "global-cuisine-bucket-final",
     metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
+      cb(null, { fieldName: "TESTING_METADATA" });
     },
     key: function (req, file, cb) {
       cb(null, Date.now().toString())
     }
   })
 })
+
+module.exports = upload;
--- routes/api/image.js
+++ routes/api/image.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const app=express()
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const router = express.Router()
 
-app.post('/images', upload.single('image') (req,res)=>{
-  res.send('yes')
+const upload = require('../../image_upload')
+
+const singleImageUpload = upload.single('image')
+router.post("/", upload.single('image'), function(req,res) {
+ singleImageUpload(req,res, function(error) {
+
+  return res.json({'imageURL':req.file.location,})
+ })
 })
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+module.exports = router;
