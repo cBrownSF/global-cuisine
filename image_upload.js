c3582ef097ac196ae912c53b37a5254593b095bc
Cal Brown <calsbrown4@gmail.com>
2021-12-22 14:48:36 -0800

    Image UPload

--- image_upload.js
+++ image_upload.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const S= require('aws-sdk');
+const AWS= require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
@@ -8,12 +8,19 @@ const multerS3 = require('multer-s3');
 // const accessKey = process.env.AWS_ACCESS_KEY;
 // const secretAccessKey = process.env.AWS_SECRET_KEY;
 
-AWS.config.update({
+// AWS.config.update({
+//   secretAccessKey: process.env.SECRET_KEY,
+//   accessKeyId: process.env.ACCESS_KEY,
+//   region: 'us-west-1'
+// })
+const s3 = new AWS.S3({
   secretAccessKey: process.env.SECRET_KEY,
   accessKeyId: process.env.ACCESS_KEY,
   region: 'us-west-1'
-})
-const s3 = new AWS.S3();
+});
+
+
+
 console.log(process.env.ACCESS_KEY)
 console.log(process.env.SECRET_KEY)
 console.log(process.env.BUCKET_NAME)
