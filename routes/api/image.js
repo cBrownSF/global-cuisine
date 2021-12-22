87d8d954f5a2ec8919f66c255718ee675cacfa91
Cal Brown <calsbrown4@gmail.com>
2021-12-22 15:22:12 -0800

    Added MiddleWare

--- routes/api/image.js
+++ routes/api/image.js
@@ -5,10 +5,9 @@ const upload = require('../../image_upload')
 
 const singleImageUpload = upload.single('image')
 router.post("/", upload.single('image'), function(req,res) {
- singleImageUpload(req,res, function(error) {
 
   return res.json({'imageURL':req.file.location,})
- })
+
 })
 
 module.exports = router;
