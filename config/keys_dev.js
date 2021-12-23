15a0a3fb7fcf0bbdb71d05823b228007ed0ee36b
Cal Brown <calsbrown4@gmail.com>
2021-12-23 01:38:12 -0800

    Change

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://globalCuisine:G8z85cssWHbcS766@cluster0.29vbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: "UltraSecret"
-};
