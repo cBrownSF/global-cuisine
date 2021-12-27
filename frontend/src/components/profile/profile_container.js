6aa5c59e5beb677b77a1eda6ffed2ec61171715b
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-12-27 12:38:28 -0800

    like profile

--- frontend/src/components/profile/profile_container.js
+++ frontend/src/components/profile/profile_container.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import Profile from "./profile";
-import { getUserListings } from "../../actions/listing_actions";
+import { getUserListings, getListings } from "../../actions/listing_actions";
 import { getLikes } from "../../actions/like_actions";
 
 
@@ -8,12 +8,14 @@ const mapStateToProps = (state) => {
   return {
     listings: Object.values(state.listings),
     currentUser: state.session.user,
+    likes: Object.values(state.likes)
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   getUserListings : (id) => dispatch(getUserListings(id)),
-  getLikes: () => dispatch(getLikes())
+  getLikes: () => dispatch(getLikes()),
+  getListings: () => dispatch(getListings())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
