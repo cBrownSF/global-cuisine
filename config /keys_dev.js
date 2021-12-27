590733913822083e9532ecc927e3c1de32a650da
your name here <roelliga@yahoo.com>
2021-12-27 10:26:19 -0800

    added config

---
+++ config /keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://globalCuisine:G8z85cssWHbcS766@cluster0.29vbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "UltraSecret"
+};
