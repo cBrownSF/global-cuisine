1c228dbaf986a2228448d214817892269f351bff
Cal Brown <calsbrown4@gmail.com>
2022-01-13 16:02:48 -0800

    filter updated

--- frontend/src/components/filter/filter.js
+++ frontend/src/components/filter/filter.js
@@ -1,24 +1,23 @@
 import React from "react";
 import FilterList from "./filter_list";
-import "./filter.css"
-
-export default class Filter extends React.Component{
-
-  constructor(props){
-    super(props)
-  
+import "./filter.css";
+export default class Filter extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
       listings: Object.values(this.props.listings),
-      listingsCopy: []
-    }
+      listingsCopy: [],
+    };
     this.handleBtns = this.handleBtns.bind(this);
   }
   handleBtns = (e) => {
     let listingsCopy;
-    if(e.target.value === "All"){
-      listingsCopy = this.props.listings
-    }else{
-      listingsCopy = this.props.listings.filter(listing => listing.country === e.target.value)
+    if (e.target.value === "All") {
+      listingsCopy = this.props.listings;
+    } else {
+      listingsCopy = this.props.listings.filter(
+        (listing) => listing.country === e.target.value
+      );
     }
     this.setState({
       listingsCopy: listingsCopy,
@@ -42,6 +41,27 @@ export default class Filter extends React.Component{
           </button>
           <button value="Italy" onClick={this.handleBtns}>
             Italy
+          </button>
+          <button value="Korea" onClick={this.handleBtns}>
+            Korea
+          </button>
+          <button value="Vietnam" onClick={this.handleBtns}>
+            Vietnam
+          </button>
+          <button value="Mexico" onClick={this.handleBtns}>
+            Mexico
+          </button>
+          <button value="Peru" onClick={this.handleBtns}>
+            Peru
+          </button>
+          <button value="China" onClick={this.handleBtns}>
+            China
+          </button>
+          <button value="Ethopia" onClick={this.handleBtns}>
+            Ethopia
+          </button>
+          <button value="Other" onClick={this.handleBtns}>
+            Other
           </button>
         </div>
         <div className="filter-recipes">
