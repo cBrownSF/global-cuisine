6c5ac9da7d851d11e8df75356d375b62804e4e0f
Cal Brown <calsbrown4@gmail.com>
2022-01-13 17:25:44 -0800

    Final Commit

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(cors)
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
